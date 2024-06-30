8997c98c9389f40d1078fdb79154d00ad5e363e8
yasser allam <yasser-allam@outlook.com>
2024-06-30 11:24:25 +0300

    added the code skeleton

---
+++ Scripts/game.js
@@ -0,0 +1,15 @@
+const buttonColors = ["red", "blue", "green", "yellow"]
+let gamePattern = [];
+let userClickedPattern = []
+let started = false
+let level = 0
+
+document.addEventListener('keypress', () => {
+
+    if (!started) {
+        document.querySelector("#level-title").textContent = `Level ${level}`
+        nextSequence();
+        started = true
+    }
+})
+
